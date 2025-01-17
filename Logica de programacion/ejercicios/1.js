42f5b1f9d13795a67dcef325b70f9bdff7f7db53
Diego Patiño <diego.unlibre@gmail.com>
2025-01-16 21:00:12 -0500

    correccion variable usuario
    Se corrige el nombre de la variable usuario

---
+++ Logica de programacion/ejercicios/1.js
@@ -0,0 +1,45 @@
+//Desafíos
+
+//Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+//Declara una variable llamada nombre y asígnale el valor "Lua".
+//Crea una variable llamada edady asígnale el valor 25.
+//Establece una variable numeroDeVentas y asígnale el valor 50.
+//Establece una variable saldoDisponible y asígnale el valor 1000.
+//Muestra una alerta con el texto "¡Error! Completa todos los campos".
+//Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+//Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+//Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+//Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
+
+
+//1
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+//2
+let nombre = "Lua";
+
+//3
+let edad = 25;
+
+//4
+let numero_de_ventas = 50;
+
+//5
+let saldo_disponible = 1000;
+
+//6
+alert("¡Error! Completa todos los campos");
+
+//7
+mensaje_de_error = "¡Error! Completa todos los campos";
+alert(mensaje_de_error);
+
+//8
+let nombre_2 = prompt("Ingresa tu nombre de usuario");
+
+//9
+let edad_2 = prompt("Ingrese su edad");
+
+if (edad_2 >= 10){
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
--- Logica de programacion/holamundo.js
+++ Logica de programacion/holamundo.js
@@ -1,7 +1,11 @@
 alert("hola mundo");
-let numero_ususario = prompt('Ingresa un número:');
+let numero_usuario = prompt('Ingresa un número:');
 let numero_secreto = 6;
 
-if (numero_usuario == numero_secreto){}
-console.log(numero_ususario);
+console.log(numero_usuario);
+
+if (numero_usuario == numero_secreto){
+    alert("Acertaste el numero secreto");
+}
+
 
