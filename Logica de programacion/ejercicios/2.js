67f7f41227341b6cc513079ce3f0017414d1e71f
Diego Patiño <diego.unlibre@gmail.com>
2025-01-19 13:52:33 -0500

    .

---
+++ Logica de programacion/ejercicios/2.js
@@ -0,0 +1,46 @@
+//Desafíos:
+
+/*
+1. Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+2. Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+3. Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.".
+4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+5. Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+*/
+
+/*1
+let dia_semana = prompt("Ingrese un dia de la semana: ");
+if (dia_semana == "sabado" || dia_semana == "domingo") {
+    alert("¡Buen fin de semana!");
+} else {
+    alert("¡Buena semana!");
+}
+*/
+
+/*2
+let numero = prompt("Ingrese un numero: ");
+if (numero >= 0){
+    alert("Su numero es positivo.");
+} else {
+    alert("Su numero es negativo.");
+}
+*/
+
+/*3
+let puntos = prompt("Ingrese una puntuacion: ");
+if (puntos >= 100){
+    alert("¡Felicidades, has ganado!");
+} else {
+    alert("Intentalo nuevamente para ganar.")
+}
+*/
+
+/*4
+let saldo = 2000000;
+alert(`El saldo de su cuenta es: ${saldo}`);
+*/
+
+/*5
+let nombre = prompt("ingrese su nombre: ");
+alert(`Bienvenid@ ${nombre}`)
+*/
--- Logica de programacion/holamundo.js
+++ Logica de programacion/holamundo.js
@@ -2,15 +2,19 @@
 alert("hola mundo");
 
 //variables
-let numero_usuario = prompt('Ingresa un número:');
-let numero_secreto = 6;
+let numero_usuario = prompt('Ingresa un número entre el 1 y el 10:');
+let numero_secreto = 5;
 
 //mostrar en la consola del navegador
 console.log(numero_usuario);
 
 //condicion para saber si el numero es igual o diferente al numero secreto
 if (numero_usuario == numero_secreto){
-    alert("Acertaste el numero secreto");
+    //concatenacion con +
+    alert("Acertaste, el numero secreto es: " + numero_usuario);
+} else {
+    //concatenacion con ``
+    alert(`No acertaste, el numero secreto es:  ${numero_secreto}`);
 }
 
 
