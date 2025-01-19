2356b305eca3a84586f657b951d1405aac544f52
Diego Patiño <diego.unlibre@gmail.com>
2025-01-19 13:08:06 -0500

    .

--- Logica de programacion/ejercicios/1.js
+++ Logica de programacion/ejercicios/1.js
@@ -1,16 +1,17 @@
 //Desafíos
 
-//Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
-//Declara una variable llamada nombre y asígnale el valor "Lua".
-//Crea una variable llamada edady asígnale el valor 25.
-//Establece una variable numeroDeVentas y asígnale el valor 50.
-//Establece una variable saldoDisponible y asígnale el valor 1000.
-//Muestra una alerta con el texto "¡Error! Completa todos los campos".
-//Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
-//Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
-//Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
-//Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
-
+/*
+1. Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+2. Declara una variable llamada nombre y asígnale el valor "Lua".
+3. Crea una variable llamada edady asígnale el valor 25.
+4. Establece una variable numeroDeVentas y asígnale el valor 50.
+5. Establece una variable saldoDisponible y asígnale el valor 1000.
+6. Muestra una alerta con el texto "¡Error! Completa todos los campos".
+7. Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+8. Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+9. Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+10. Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
+*/
 
 //1
 alert("¡Bienvenida y bienvenido a nuestro sitio web!");
@@ -40,6 +41,7 @@ let nombre_2 = prompt("Ingresa tu nombre de usuario");
 //9
 let edad_2 = prompt("Ingrese su edad");
 
+//10
 if (edad_2 >= 10){
     alert("¡Puedes obtener tu licencia de conducir!");
 }
--- Logica de programacion/holamundo.js
+++ Logica de programacion/holamundo.js
@@ -1,9 +1,14 @@
+//print
 alert("hola mundo");
+
+//variables
 let numero_usuario = prompt('Ingresa un número:');
 let numero_secreto = 6;
 
+//mostrar en la consola del navegador
 console.log(numero_usuario);
 
+//condicion para saber si el numero es igual o diferente al numero secreto
 if (numero_usuario == numero_secreto){
     alert("Acertaste el numero secreto");
 }
