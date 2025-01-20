b0fa0908f85ee2d7e84f85cb4c24bf7dd6d3f7e1
Diego Patiño <diego.unlibre@gmail.com>
2025-01-20 00:19:58 -0500

    coreccion de contador

---
+++ Logica de programacion/ejemplos/2.js
@@ -0,0 +1,9 @@
+//Operador termario
+
+let palabraPersona = cantidadPersonas == 1 ? "persona" : "personas";
+
+if(cantidadPersonas == 1){
+    palabraPersona = "persona";
+}else{
+    palabraPersona = "personas"
+}
--- Logica de programacion/holamundo.js
+++ Logica de programacion/holamundo.js
@@ -5,33 +5,37 @@ alert("hola mundo");
 let numero_secreto = 5;
 let numero_usuario = 0;
 let intentos = 0;
+let intentos_maximos = 4;
 
 while(numero_usuario != numero_secreto){
 
     //Aumentos de intentos
-    intentos += 1;
+    intentos ++;
+    // intentos = intentos + 1;
+    //intentos += 1;
+
     let numero_usuario = prompt('Ingresa un número entre el 1 y el 10:');
 
     //mostrar en la consola del navegador
     console.log(numero_usuario);
 
     //condicion para saber si el numero es igual o diferente al numero secreto
-    if (numero_usuario == numero_secreto){
+    if (intentos >= intentos_maximos){
+        //concatenacion con ``
+        alert(`No acertaste por maximo de ${intentos_maximos} intentos alcanzados, el numero secreto es:  ${numero_secreto}`);
+        break;
+
+    } else if (numero_usuario == numero_secreto){
         //concatenacion con +
         alert("Acertaste, el numero secreto es: " + numero_usuario + " Intentos realizados: " + intentos);
         break;
-
+        
     } else if (numero_usuario < numero_secreto){
         //concatenacion con ``
-        alert(`El numero secreto es mayor que ${numero_usuario}. `);
-        
+        alert(`El numero secreto es mayor que ${numero_usuario}. `);   
+
     } else if (numero_usuario > numero_secreto){
         //concatenacion con +
-        alert("El numero secreto es menor que: " + numero_usuario);   
-
-    } else if (intentos >= 4){
-        //concatenacion con ``
-        alert(`No acertaste por maximo intentos alcanzados, el numero secreto es:  ${numero_secreto}`);
-        break;
+        alert("El numero secreto es menor que: " + numero_usuario);
     }
 }
