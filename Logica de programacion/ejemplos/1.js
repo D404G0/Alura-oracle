2a7b7d4cb85acc7be1b38c6c21455e718b35660b
Diego Patiño <diego.unlibre@gmail.com>
2025-01-19 20:32:17 -0500

    Se crean mas ejercicios y se a;ade un ejemplo

---
+++ Logica de programacion/ejemplos/1.js
@@ -0,0 +1,18 @@
+//AND &&
+let edad = 25;
+let tieneLicencia = true;
+
+// si la edad es mayor de 18 y tiene una licencia...
+if (edad > 18 && tieneLicencia) {
+  console.log("Puede conducir!");
+} else {
+  console.log("No puede conducir!");
+}
+
+//OR
+// si tiene manzana o tiene banama…
+if (tieneManzana|| tieneBanana) {
+    console.log("Tienes frutas!");
+  } else {
+    console.log("No tienes frutas.");
+  }
--- Logica de programacion/holamundo.js
+++ Logica de programacion/holamundo.js
@@ -2,26 +2,39 @@
 alert("hola mundo");
 
 //variables
-let numero_usuario = prompt('Ingresa un número entre el 1 y el 10:');
 let numero_secreto = 5;
+let numero_usuario = 0;
+let intentos = 1;
 
-//mostrar en la consola del navegador
-console.log(numero_usuario);
+while(numero_usuario != numero_secreto){
+    let numero_usuario = prompt('Ingresa un número entre el 1 y el 10:');
 
-//condicion para saber si el numero es igual o diferente al numero secreto
-if (numero_usuario == numero_secreto){
-    //concatenacion con +
-    alert("Acertaste, el numero secreto es: " + numero_usuario);
+    //mostrar en la consola del navegador
+    console.log(numero_usuario);
 
-} else if (numero_usuario < numero_secreto){
-    alert(`El numero secreto es mayor al que ingreso: ${numero_usuario}`);
+    //condicion para saber si el numero es igual o diferente al numero secreto
+    if (numero_usuario == numero_secreto){
+        //concatenacion con +
+        alert("Acertaste, el numero secreto es: " + numero_usuario + " Intentos realizados: " + intentos);
 
-} else if (numero_usuario > numero_secreto){
-    alert(`El numero secreto es menor al que ingreso: ${numero_usuario}`);
-    
-} else {
-    //concatenacion con ``
-    alert(`No acertaste, el numero secreto es:  ${numero_secreto}`);
+    } else if (numero_usuario < numero_secreto){
+        //Aumentos de intentos
+        intentos = intentos + 1;
+        //concatenacion con ``
+        alert(`El numero secreto es mayor que ${numero_usuario}. `);
+        
+    } else if (numero_usuario > numero_secreto){
+        //Aumentos de intentos
+        intentos = intentos + 1;
+        //concatenacion con +
+        alert("El numero secreto es menor que: " + numero_usuario);
+        
+    } else {
+        //concatenacion con ``
+        alert(`No acertaste, el numero secreto es:  ${numero_secreto}`);
+    }
+
+
+
 }
-
-
+    
