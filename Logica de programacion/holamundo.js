ea1924c19ac755b54842738dd39b567d4165ee2b
Diego Patiño <diego.unlibre@gmail.com>
2025-01-20 18:11:50 -0500

    Juego finalizado

--- Logica de programacion/holamundo.js
+++ Logica de programacion/holamundo.js
@@ -1,43 +1,59 @@
-//print
-alert("hola mundo");
+    //print
+    alert("hola mundo");
 
-//variables
-let numero_secreto = Math.floor(Math.random() * 10) + 1;
-let numero_usuario = 0;
-let intentos = 0;
-let intentos_maximos = 4;
+    //prompt
+    let rango_numero = parseInt(prompt("Ingresa un numero maximo para generar un numero secreto: "))
+    console.log(rango_numero);
 
-while(numero_usuario != numero_secreto){
-
-    //Aumentos de intentos
-    intentos ++;
-    // intentos = intentos + 1;
-    //intentos += 1;
-
-    //input del numero del usuario
-    let numero_usuario = parseInt(prompt('Ingresa un número entre el 1 y el 10: '));
-
-    //mostrar en la consola del navegador
-    console.log(typeof(numero_usuario));
+    //variables
+    let numero_secreto = Math.floor(Math.random() * rango_numero) + 1;
     console.log(numero_secreto);
 
-    //condicion para saber si el numero es igual o diferente al numero secreto
-    if (numero_usuario == numero_secreto){
-        //concatenacion con +
-        alert("Acertaste, el numero secreto es: " + numero_usuario + ". Intentos realizados: " + intentos);
-        break;
+while (true){
+    let numero_usuario = 0;
+    let intentos = 0;
 
-    } else if (intentos >= intentos_maximos){
-        //concatenacion con ``
-        alert(`No acertaste por maximo de ${intentos_maximos} intentos alcanzados, el numero secreto es:  ${numero_secreto}`);
-        break;
+    let intentos_maximos = parseInt(prompt("Ingrese en cuantos intentos quiere tener, no mas de " + (rango_numero/2)));
+    console.log(intentos_maximos);
+    if (intentos_maximos <= (rango_numero/2)){
+        while(numero_usuario != numero_secreto){
+    
+            //Aumentos de intentos
+            intentos ++;
+            // intentos = intentos + 1;
+            //intentos += 1;
         
-    } else if (numero_usuario < numero_secreto){
-        //concatenacion con ``
-        alert(`El numero secreto es mayor que ${numero_usuario}. `);   
-
-    } else if (numero_usuario > numero_secreto){
-        //concatenacion con +
-        alert("El numero secreto es menor que: " + numero_usuario); 
+            //input del numero del usuario
+            let numero_usuario = parseInt(prompt(`Ingresa un número entre el 1 y el ${rango_numero}: `));
+        
+            //mostrar en la consola del navegador
+            //console.log(typeof(numero_usuario));
+        
+            //condicion para saber si el numero es igual o diferente al numero secreto
+            if (numero_usuario == numero_secreto){
+                //concatenacion con +
+                alert("Acertaste, el numero secreto es: " + numero_usuario + ". Intentos realizados: " + intentos);
+                break;
+        
+            } else if (intentos >= intentos_maximos){
+                //concatenacion con ``
+                alert(`No acertaste por maximo de ${intentos_maximos} intentos alcanzados, el numero secreto es:  ${numero_secreto}`);
+                break;
+                
+            } else if (numero_usuario < numero_secreto){
+                //concatenacion con ``
+                alert(`El numero secreto es mayor que ${numero_usuario}. `);   
+        
+            } else if (numero_usuario > numero_secreto){
+                //concatenacion con +
+                alert("El numero secreto es menor que: " + numero_usuario); 
+            }
+        }
+        break
+    
+    } else {
+        alert("Error, intentelo nuevamente")
     }
+
 }
+
