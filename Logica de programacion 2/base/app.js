45e439abf5a2a4daeb8b1a4530576fbdc8132bef
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-21 23:40:17 -0500

    Update app.js

--- Logica de programacion 2/base/app.js
+++ Logica de programacion 2/base/app.js
@@ -1,10 +1,19 @@
+let numero_secreto = generar_numero_secreto();
+console.log(numero_secreto)
+
 function texto_para_elemento(elemento, texto){
     let variableHTML = document.querySelector(elemento);
     variableHTML.innerHTML = texto;
+    return;
 }
 
 function intento_de_ususario(){
     alert("Clik desde el boton usando la funcion.");
+    return;
+}
+
+function generar_numero_secreto(){
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 texto_para_elemento("h1", "Adivina el numero secreto:D.");
