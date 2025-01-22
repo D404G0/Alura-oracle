b88bbf68c9748c71a4bf1d8b19bee35b92ff90b4
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-21 23:33:01 -0500

    se reducer las lineas de codigo en el archivo js

--- Logica de programacion 2/base/app.js
+++ Logica de programacion 2/base/app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Adivina el numero secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Ingresa un numero maximo para generar un numero secreto: ";
+function texto_para_elemento(elemento, texto){
+    let variableHTML = document.querySelector(elemento);
+    variableHTML.innerHTML = texto;
+}
 
 function intento_de_ususario(){
     alert("Clik desde el boton usando la funcion.");
 }
+
+texto_para_elemento("h1", "Adivina el numero secreto:D.");
+texto_para_elemento("p", "Ingresa un numero maximo para generar un numero secreto :D : ");
