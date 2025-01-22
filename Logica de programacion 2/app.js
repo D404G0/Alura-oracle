f039830cfc85b4d2306c6420e99e8647dbaf65a1
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-21 22:47:24 -0500

    conexion de archivo js con html

---
+++ Logica de programacion 2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Adivina el numero secreto'
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Ingresa un numero de 1 al 10."
