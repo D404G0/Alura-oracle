440eb5df284644230db0493e6ce7e7bd597a9e61
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-21 22:59:27 -0500

    se organiza el contenido en carpetas respectivas

--- Logica de programacion 2/app.js
+++
@@ -1,5 +0,0 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Adivina el numero secreto'
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Ingresa un numero de 1 al 10."
---
+++ Logica de programacion 2/base/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Adivina el numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Ingresa un numero maximo para generar un numero secreto: ";
+
+function intento_de_ususario(){
+    alert("Clik desde el boton usando la funcion.")
+}
