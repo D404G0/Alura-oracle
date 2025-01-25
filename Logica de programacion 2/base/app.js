76c95eea472677e824607311a585b7bf9f380ab2
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-24 22:04:45 -0500

    se ajusta el diseño de los botones y se estructura la condicion en caso de acertar el numero

--- Logica de programacion 2/base/app.js
+++ Logica de programacion 2/base/app.js
@@ -1,5 +1,9 @@
 let numero_secreto = generar_numero_secreto();
 
+function generar_numero_secreto(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+
 
 function texto_para_elemento(elemento, texto){
     let variableHTML = document.querySelector(elemento);
@@ -7,18 +11,19 @@ function texto_para_elemento(elemento, texto){
     return;
 }
 
+texto_para_elemento("h1", "Adivina el numero secreto:D.");
+texto_para_elemento("p", "Ingresa un numero maximo para generar un numero secreto :D. ");
+
 function verificar_intento(){
     let numero_usuario = parseInt(document.getElementById('numero_usuario').value);
-    console.log(typeof(numero_usuario));
-    console.log(numero_secreto);
-    console.log(numero_usuario);
-    console.log(numero_secreto === numero_usuario);
+    if(numero_secreto === numero_usuario){
+        texto_para_elemento("p", `Acertaste, el numero secreto es ${numero_usuario}. `); 
+
+    }else if(numero_usuario > numero_secreto){
+        texto_para_elemento("p", `El numero secreto es menor que ${numero_usuario}.`);
+
+    }else if(numero_usuario < numero_secreto){
+        texto_para_elemento("p", `El numero secreto es mayor que ${numero_usuario}. `);
+    }
     return;
 }
-
-function generar_numero_secreto(){
-    return Math.floor(Math.random() * 10) + 1;
-}
-
-texto_para_elemento("h1", "Adivina el numero secreto:D.");
-texto_para_elemento("p", "Ingresa un numero maximo para generar un numero secreto :D : ");
