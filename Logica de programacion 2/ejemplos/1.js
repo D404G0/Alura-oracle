117950abcebecf69b06704b0eceab46663781449
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-28 00:49:39 -0500

    Se crea la carpeta de ejemplo y se agrega un archivo js

---
+++ Logica de programacion 2/ejemplos/1.js
@@ -0,0 +1,10 @@
+let frutas = ["Manzana", "Uva", "Naranja"];
+
+console.log(frutas[0]); // Salida: "Manzana"
+console.log(frutas[2]); // Salida: "Naranja"
+
+frutas.push("Fresa");
+console.log(frutas); // Salida: ["Manzana", "Uva", "Naranja", "Fresa"]
+
+frutas.pop();
+console.log(frutas); // Salida: ["Manzana", "Uva", "Naranja"]
