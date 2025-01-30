4c20c01f5fb5486a2138cdca630694a75e287c63
Diego Patiño <diegol-patinof@unilibre.edu.co>
2025-01-30 16:26:35 -0500

    Delete Logica de programacion directory

--- Logica de programacion/ejemplos/2.js
+++
@@ -1,9 +0,0 @@
-//Operador termario
-
-let palabraPersona = cantidadPersonas == 1 ? "persona" : "personas";
-
-if(cantidadPersonas == 1){
-    palabraPersona = "persona";
-}else{
-    palabraPersona = "personas"
-}
